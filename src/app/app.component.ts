b2d1a19d831a77ba10f11fd353d2d8f1157ad5f1
David liu <david.liu@clarifyhealth.com>
2021-04-08 17:33:24 +0800

    Added mock scene to reproduce the jumping table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
-
+import { Component, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import { TableVirtualScrollDataSource } from 'ng-table-virtual-scroll';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'mat-table-virtual-scroll-issue';
+  columnNames: any[] = [];
+  tableRows = new TableVirtualScrollDataSource();
+  columns = [];
+  dataRows = [];
+
+  rowCount = 1;
+  columnCount = 1;
+
+  colorClasses = ['cell-color-red', 'cell-color-green', 'cell-color-blue'];
+
+  constructor(private cdr: ChangeDetectorRef) {
+
+  }
+
+  rowChange(value: number): void {
+    this.getNewTableRowAndColumns();
+  }
+
+  columnsChange(value: number): void {
+    this.getNewTableRowAndColumns();
+  }
+
+  getNewTableRowAndColumns(): void {
+    this.columnNames = [...Array(this.columnCount).keys()].map((value: any, index: any) => {
+      return 'Column' + index;
+    });
+
+    this.dataRows = [...Array(this.rowCount).keys()].map((value, index) => {
+      const row = {};
+      this.columnNames.forEach((column: any) => {
+        row[column] = {
+          value: Math.floor((Math.random() * 100)),
+          cellClass: this.colorClasses[(Math.ceil((Math.random() * 300) / 100)) - 1]
+        };
+      });
+      return row;
+    });
+    this.tableRows.data = this.dataRows;
+    this.cdr.detectChanges();
+    console.log(this.tableRows.data);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material/table';
+import { TableVirtualScrollModule } from 'ng-table-virtual-scroll';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { MatInputModule } from '@angular/material/input';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatInputModule,
+    ScrollingModule,
+    TooltipModule.forRoot(),
+    TableVirtualScrollModule
   ],
   providers: [],
   bootstrap: [AppComponent]
